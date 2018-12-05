b72fb53531252c7fa7883f289f10fcb2364df8e6
Tooba3Naseer <42033832+Tooba3Naseer@users.noreply.github.com>
2018-12-05 17:49:01 +0500

    Update HotelDatasController.cs

--- Hotel/Hotel/Controllers/HotelDatasController.cs
+++ Hotel/Hotel/Controllers/HotelDatasController.cs
@@ -27,6 +27,33 @@ namespace Hotel.Controllers
             return View();
         }
 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddHotel([Bind(Include = "HotelID,HotelName,Ratings,Category,FreeWifi,PriceRangeUpper, PriceRangeLower,RoomAvailable,SwimmingPool,CarPark,FreeBreakfast,PrivateParking, PlayLand")] HotelData Hotelobj)
+        {
+            try
+            {
+
+
+
+                if (ModelState.IsValid)
+                {
+                    db.HotelDatas.Add(Hotelobj);
+                    db.SaveChanges();
+
+                }
+            }
+
+            catch (DataException)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+
+            return View(Hotelobj);
+        }
+
         // GET: HotelDatas/Details/5
         public ActionResult Details(int? id)
         {
