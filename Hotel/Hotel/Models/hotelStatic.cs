5b62e79c84e8af0e83365011495844f6cfadb4c5
Tooba3Naseer <42033832+Tooba3Naseer@users.noreply.github.com>
2018-12-07 21:04:57 +0500

    Create hotelStatic.cs

---
+++ Hotel/Hotel/Models/hotelStatic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotel.Models
+{
+    public static class HotelStatic
+    {
+        public static int Hotelid { get; set; }
+        public static string Hotelname { get; set; }
+    }
+}
