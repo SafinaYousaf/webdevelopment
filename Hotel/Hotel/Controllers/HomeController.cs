45081576e108b54b55ee2c7a3a369a67cdb2ae97
Hina <hinamaqsood378@gmail.com>
2018-12-08 14:51:53 +0500

    ...

--- Hotel/Hotel/Controllers/HomeController.cs
+++ Hotel/Hotel/Controllers/HomeController.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Data.SqlClient;
 using Hotel.Models;
-using System.ComponentModel.DataAnnotations;
 using System.Configuration;
-using System.Data.Entity.Validation;
-
-using System.Net;
 
 namespace Hotel.Controllers
 {
--- Hotel/Hotel/Controllers/HotelDatasController.cs
+++ Hotel/Hotel/Controllers/HotelDatasController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using Hotel;
 using Hotel.Models;
 
 namespace Hotel.Controllers
