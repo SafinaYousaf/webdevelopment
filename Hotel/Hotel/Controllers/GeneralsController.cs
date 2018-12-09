709bf30a3a4a3ccaa76a6808a46255c6b73f61ac
SafinaYousaf <41517325+SafinaYousaf@users.noreply.github.com>
2018-12-09 16:03:19 +0500

    ....

--- Hotel/Hotel/Controllers/GeneralsController.cs
+++ Hotel/Hotel/Controllers/GeneralsController.cs
@@ -28,27 +28,147 @@ namespace Hotel.Controllers
         {
             return View();
         }
-        public ActionResult user()
-        {
-            return View();
-        }
-        public ActionResult feedback()
-        {
-            return View();
-        }
-        public ActionResult favourites()
-        {
-            return View();
-        }
-        public ActionResult map()
-        {
-            return View();
-        }
 
         public ActionResult Login()
         {
             return View();
         }
+
+        public ActionResult favourites()
+        {
+            return View();
+        }
+
+        public ActionResult user()
+        {
+            return View();
+        }
+
+        public ActionResult map()
+        {
+            return View();
+        }
+        //feedback
+        public ActionResult feedback()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult feedback([Bind(Include = "UserName,HotelName,comment,Id")] FeedBack obj)
+        {
+
+            try
+            {
+
+                obj.UserName = HotelStatic.username;
+
+
+                if (ModelState.IsValid)
+                {
+                    db.FeedBacks.Add(obj);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Dashboard");
+                }
+            }
+
+            catch (DataException)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+
+            }
+            return View(obj);
+
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(General entity)
+        {
+            using (HotelEntities db = new HotelEntities())
+            {
+                //fetche the data of user on the basis of inserted email
+                General user = db.Generals.FirstOrDefault(u => u.Email == (entity.Email));
+                //if entered email is not bound to any user then object will be null
+                if (user == null)
+                {
+                    TempData["ErrorMSG"] = "object not found";
+                    return View(entity);
+
+                }
+                //if we are here then it mean we sucsessfuly retrived the data
+                //now we compare password
+                int a = entity.Password.Count();
+                if (user.Password.Substring(0, a) != entity.Password)
+                {
+                    TempData["ErrorMSG"] = "Password not matched";
+                    return View(entity);
+
+                }
+
+                if (user != null)
+                {
+                    
+
+                    if (user.Password.Substring(0, a) == entity.Password)
+                    {
+
+                       // var Users = new UserStatic();
+                        if (user.Type.Substring(0, 5) == "Admin")
+                            return RedirectToAction("AdminDash", "HotelDatas");
+                        if (user.Type.Substring(0, 12) == "HotelManager")
+                        {
+
+                            // first add hotel by hotel manger then hotelmanger will able to see dashboard
+                            return RedirectToAction("AddHotel1", "HotelDatas");
+                        }
+                        if (user.Type.Substring(0, 4) == "User")
+                        {
+                            HotelStatic.username = user.Name;
+                            HotelStatic.Id = user.ID;
+                            return RedirectToAction("Dashboard");
+                        }
+                    }
+                    return View();
+                }
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult user([Bind(Include = "FirstName,LastName,Adress,City,Country,PostalCode,About,UserID,ID")] UserData obj)
+        {
+
+            try
+            {
+
+                obj.ID = HotelStatic.Id;
+
+
+                if (ModelState.IsValid)
+                {
+                    db.UserDatas.Add(obj);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Dashboard");
+                }
+            }
+
+            catch (DataException)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+
+            }
+            return View(obj);
+
+
+        }
+
+
 
         // GET: Generals/Details/5
         public ActionResult Details(string id)
@@ -95,9 +215,10 @@ namespace Hotel.Controllers
                 {
                     db.Generals.Add(general);
                     db.SaveChanges();
+                    
                     //after saving data in database
                     //we check for type of user and direct it to respective database
-                    if(general.Type == "Admin")
+                    if (general.Type == "Admin")
                         return RedirectToAction("AdminDash", "HotelDatas");
                     if (general.Type == "HotelManager")
                     {
@@ -105,7 +226,11 @@ namespace Hotel.Controllers
                         return RedirectToAction("AddHotel1", "HotelDatas");
                     }
                     if (general.Type == "User")
+                    {
+                        HotelStatic.username = general.Name;
+                        HotelStatic.Id = user.ID;
                         return RedirectToAction("Dashboard");
+                    }
                 }
             }
 
@@ -175,54 +300,7 @@ namespace Hotel.Controllers
             return RedirectToAction("Index");
         }
         
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Login(General entity)
-        {
-            using (HotelEntities db = new HotelEntities())
-            {
-                //fetche the data of user on the basis of inserted email
-                General user = db.Generals.FirstOrDefault(u => u.Email == (entity.Email));
-                //if entered email is not bound to any user then object will be null
-                if (user == null)
-                {
-                    TempData["ErrorMSG"] = "object not found";
-                    return View(entity);
-
-                }
-                //if we are here then it mean we sucsessfuly retrived the data
-                //now we compare password
-                int a = entity.Password.Count();
-                if (user.Password.Substring(0, a) != entity.Password)
-                {
-                    TempData["ErrorMSG"] = "Password not matched";
-                    return View(entity);
-
-                }
-
-                if (user != null)
-                {
-
-                    if (user.Password.Substring(0, a) == entity.Password)
-                    {
-
-
-                        if (user.Type.Substring(0, 5) == "Admin")
-                            return RedirectToAction("AdminDash", "HotelDatas");
-                        if (user.Type.Substring(0, 12) == "HotelManager")
-                        {
-
-                            // first add hotel by hotel manger then hotelmanger will able to see dashboard
-                            return RedirectToAction("AddHotel1", "HotelDatas");
-                        }
-                        if (user.Type.Substring(0, 4) == "User")
-                            return RedirectToAction("Dashboard");
-                    }
-                    return View();
-                }
-                return View();
-            }
-        }
+        
         //Logout//
         public ActionResult Logout()
         {
